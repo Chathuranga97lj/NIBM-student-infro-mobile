b3b18083e5f98fc0422a09a2ab25346a881941d2
Chathu97 <chathuit97@gmail.com>
2023-11-26 13:05:57 +0530

    create login and register page

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:student_info/Register.dart';
+import 'Login.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -15,8 +16,72 @@ class _HomeViewState extends State<HomeView> {
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('NIBM Student Management'),
+        foregroundColor: Colors.white,
         centerTitle: true,
-        backgroundColor: Colors.blueAccent,
+        backgroundColor: Color.fromRGBO(45, 68, 139, 1),
+        automaticallyImplyLeading: false,
+      ),
+      body: Column(
+        children: [
+          Padding(padding: const EdgeInsets.all(10.0),
+            child: Image.asset('assets/image/nibm.png'),
+          ),
+          Text('Kurunegala Branch',
+          style: TextStyle(
+            color: Color.fromRGBO(45, 68, 139, 1),
+            fontSize: 30
+          ),
+          ),
+          Padding(padding: const EdgeInsets.all(1.0),
+            child: Image.asset('assets/image/home.jpg', height: 300, width: 300,),
+          ),
+          Padding(padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Login()),
+                );
+              },
+             style: ElevatedButton.styleFrom(
+               primary: Color.fromRGBO(45, 68, 139, 1)
+             ),
+              child: const Center(
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Padding(padding: const EdgeInsets.all(20.0),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Register()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                  primary: Color.fromRGBO(45, 68, 139, 1)
+              ),
+              child: const Center(
+                child: Text(
+                  'Register',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/Login.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+        foregroundColor: Colors.white,
+        centerTitle: true,
+        backgroundColor: Color.fromRGBO(45, 68, 139, 1),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/image/home.jpg', height: 150, width: 150),
+            TextField(
+              controller: usernameController,
+              decoration: InputDecoration(
+                labelText: 'Username',
+              ),
+            ),
+            SizedBox(height: 16.0),
+            TextField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                // You can add your login logic here.
+                // For now, let's just print the username and password.
+                print('Username: ${usernameController.text}');
+                print('Password: ${passwordController.text}');
+              },
+              style: ElevatedButton.styleFrom(
+                  primary: Color.fromRGBO(45, 68, 139, 1)
+              ),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
